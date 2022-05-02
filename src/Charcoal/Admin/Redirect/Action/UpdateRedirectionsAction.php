7a713f07496680bcbe0b3a8758b0e50b941cbb39
Joel Alphonso <joel@locomotive.ca>
2022-05-02 14:46:33 -0400

    chore: add action to update redirections

---
+++ src/Charcoal/Admin/Redirect/Action/UpdateRedirectionsAction.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Charcoal\Admin\Redirect\Action;
+
+use Charcoal\Admin\AdminAction;
+use Charcoal\Redirect\Service\RedirectionService;
+use Pimple\Container;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+
+/**
+ * Update Redirections Action
+ */
+class UpdateRedirectionsAction extends AdminAction
+{
+    /**
+     * @var RedirectionService
+     */
+    protected RedirectionService $redirectionService;
+
+    /**
+     * Set common dependencies used in all admin actions.
+     *
+     * @param Container $container DI Container.
+     * @return void
+     */
+    protected function setDependencies(Container $container)
+    {
+        parent::setDependencies($container);
+
+        $this->redirectionService = $container['charcoal/redirection/service'];
+    }
+
+    /**
+     * Gets a psr7 request and response and returns a response.
+     *
+     * Called from `__invoke()` as the first thing.
+     *
+     * @param RequestInterface  $request  A PSR-7 compatible Request instance.
+     * @param ResponseInterface $response A PSR-7 compatible Response instance.
+     * @return ResponseInterface
+     */
+    public function run(RequestInterface $request, ResponseInterface $response): ResponseInterface
+    {
+        $data = $request->getParams();
+
+        $this->redirectionService->updateRedirections($data);
+
+        $this->setSuccess(true);
+        return $response;
+    }
+}
